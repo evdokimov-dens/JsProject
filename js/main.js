41c53513566a221ea926fad59ca98b76422a2ec0
Denis Evdokimov <evdokimov.denis96@yandex.ru>
2026-04-08 22:49:55 +0500

    add commit on lesson02

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "myProject";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 16;
+let rollback = 33;
+let fullPrice = 6480;
+let adaptive = true;
 
-alert("Hello world!");
-console.log("Hello log");
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback / 100));
