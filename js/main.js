674983d9190f4612c0418107d03e31df6b479f2b
Denis Evdokimov <evdokimov.denis96@yandex.ru>
2026-04-09 22:18:19 +0500

    performed the first calculations and conditional branching

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-let title = "myProject";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 16;
+let title = prompt("Как называется проект?", "Интернет-магазин");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "200");
 let rollback = 33;
-let fullPrice = 6480;
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?", "PixelPerfect");
+let servicePrice1 = +prompt("Сколько это будет стоить?", "230");
+let service2 = prompt("Какой дополнительный тип услуги нужен?", "Мобильная разработка");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "400");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 console.log(title);
 console.log(fullPrice);
@@ -13,3 +18,14 @@ console.log(`Стоимость верстки экранов ${screenPrice} р�
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(","));
 console.log(fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так");
+}
